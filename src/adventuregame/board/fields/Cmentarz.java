ea81177361695e3e195326eabaec2195abb47397
Krystian Fejku <przecinek45@o2.pl>
2016-06-08 21:02:05 +0200

    TODO: -FieldGetCard -Kapliczka -Pola -Puszcza -Rowniny -Ruiny -Straznik -Wioska -Wzgorza
    Done:
    -Gospoda
    -Miasto
    -Las
    -Skały
    -Cmentarz

--- src/adventuregame/board/fields/Cmentarz.java
+++ src/adventuregame/board/fields/Cmentarz.java
@@ -22,19 +22,23 @@ public class Cmentarz extends Field{
         } else if (actualExplorer.getCharacter() == ExplorerCharacter.ZLY) {
             switch(board.getDice().throwDice()) {
                 case 1:
-                        //1. Tracisz 1 turę.
-                        actualExplorer.loseLife();
-                        break;
+                    //1. Tracisz 1 turę.
+                    board.getDialog().message("Tracisz 1 turę.");
+                    actualExplorer.loseLife();
+                    break;
                 case 2: 
                 case 3:
-                        //2-3. Leczysz 1 punkt Wytrzymałości
-                        actualExplorer.regainLife();
-                        break;
+                    //2-3. Leczysz 1 punkt Wytrzymałości
+                    board.getDialog().message("Leczysz 1 punkt Wytrzymałości.");
+                    actualExplorer.regainLife();
+                    break;
                 case 4:
                 case 5:
                 case 6:       
-                        //TODO: 4-6 Zyskujesz 1 Czar 
-                        break;
+                    //4-6. Zyskujesz 1 Czar 
+                    board.getDialog().message("Zyskujesz 1 Czar.");
+                    //explorers.getActualExplorer().gainSpell(board);
+                    break;
             }
         }
     }
