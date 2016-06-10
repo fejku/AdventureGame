994553805613cc5eb79172d03701417352ae668c
Krystian Fejku <przecinek45@o2.pl>
2016-06-10 22:09:49 +0200

    Actualy working on: -Items
    TODO:
    -FieldGetCard
    -Pola
    -Puszcza
    -Rowniny
    -Ruiny
    -Straznik
    -Wzgorza
    
    Done:
    -Gospoda
    -Miasto
    -Las
    -Skały
    -Cmentarz
    -Kapliczk
    -Wioskaa

--- src/adventuregame/explorer/Test.java
+++ src/adventuregame/explorer/Test.java
@@ -9,7 +9,7 @@ package adventuregame.explorer;
  *
  * @author Fake
  */
-public class Test extends Explorer{
+public class Test extends Explorer {
     
     public Test(String name, int baseStrength, int baseCraft, int startingPosition, ExplorerCharacter character) {
         super(name, baseStrength, baseCraft, startingPosition, character);
