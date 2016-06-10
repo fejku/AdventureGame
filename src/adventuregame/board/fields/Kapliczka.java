8cbd38072d6a65f61aedbcaa07bb373d03ea040c
Krystian Fejku <przecinek45@o2.pl>
2016-06-10 22:14:14 +0200

    Actualy working on: -Items
    TODO:
    -FieldGetCard
    -Pola
    -Puszcza
    -Rowniny
    -Ruiny
    -Straznik
    -Wzgorza
    
    Done:
    -Gospoda
    -Miasto
    -Las
    -Skały
    -Cmentarz
    -Kapliczk
    -Wioskaa

--- src/adventuregame/board/fields/Kapliczka.java
+++ src/adventuregame/board/fields/Kapliczka.java
@@ -7,8 +7,9 @@ package adventuregame.board.fields;
 
 import adventuregame.board.Board;
 import adventuregame.explorer.Explorer;
-import adventuregame.explorer.ExplorerCharacter;
 import adventuregame.explorer.Explorers;
+import adventuregame.utils.Dice;
+import adventuregame.utils.IDialog;
 
 /**
  *
@@ -24,48 +25,80 @@ public class Kapliczka extends Field {
         Explorer actualExplorer = explorers.getActualExplorer();
         switch(actualExplorer.getCharacter()) {
             case ZLY:
-                board.getDialog().message("Tracisz 1 punkt wytrzymałości.");
-                actualExplorer.loseLife();
+            	evilAction(board.getDialog(), actualExplorer);
                 break;
             case NEUTRALNY:
-                if ((explorers.getActualExplorer().getGold() > 0) && (explorers.getActualExplorer().getLife() < 4)) {
-                    board.getDialog().message("Możesz zostać wyleczony z ran, płacąc 1 mieszek złota za 1 odzyskany punkt Wytrzymałości.");
-                    if(board.getDialog().chooseYesNo("Korzystasz?")) {
-                        explorers.getActualExplorer().loseGold();
-                        explorers.getActualExplorer().gainLife();
-                    }
-                }
+            	neutralAction(board.getDialog(), actualExplorer);
                 break;
             case DOBRY:
+            	goodAction(board.getDialog(), board.getDice(), actualExplorer);
                 break;
                 
         }
-//        if (actualExplorer.getCharacter() == ExplorerCharacter.ZLY) {
-//            
-//        } else if*()
-//        else if (actualExplorer.getCharacter() == ExplorerCharacter.ZLY) {
-//            switch(board.getDice().throwDice()) {
-//                case 1:
-//                    //1. Tracisz 1 turę.
-//                    board.getDialog().message("Tracisz 1 turę.");
-//                    actualExplorer.loseLife();
-//                    break;
-//                case 2: 
-//                case 3:
-//                    //2-3. Leczysz 1 punkt Wytrzymałości
-//                    board.getDialog().message("Leczysz 1 punkt Wytrzymałości.");
-//                    actualExplorer.regainLife();
-//                    break;
-//                case 4:
-//                case 5:
-//                case 6:       
-//                    //4-6. Zyskujesz 1 Czar 
-//                    board.getDialog().message("Zyskujesz 1 Czar.");
-//                    //explorers.getActualExplorer().gainSpell(board);
-//                    break;
-//            }
-//        }
     }
     
+    private void evilAction(IDialog dialog, Explorer explorer) {
+        dialog.message("Tracisz 1 punkt wytrzymałości.");
+        explorer.loseLife();
+    }
     
+    private void neutralAction(IDialog dialog, Explorer explorer) {
+    	//Ustalanie maksymalnej ilości życia jaką można odzyskać i na ile pozwala posiadane złoto
+    	int maxRegainLife = explorer.getMaxRegainLife();
+    	//Można odzyskać życie
+        if (maxRegainLife > 0) {
+        	dialog.message("Możesz zostać wyleczony z ran, płacąc 1 mieszek złota za 1 odzyskany punkt Wytrzymałości.");
+            if(dialog.chooseYesNo("Korzystasz?")) {
+            	//Tylko jeden mieszek lub 1 ustracony punkt Wytrzymałości
+            	if (maxRegainLife == 1) {
+            		explorer.loseGold();
+            		explorer.regainLife();
+            	} else {            	
+            		//Lista możliwych do odzyskania punktów Wytrzymałości 
+	            	String[] lifeAmount = new String[maxRegainLife];
+	            	for (int i = 1; i < maxRegainLife; i++)
+	            		lifeAmount[i] = Integer.toString(i);
+	            	int choise = dialog.chooseOption("Ile punktów zdrowia chcesz wyleczyć?", lifeAmount);
+	            	explorer.loseGold(choise);
+	            	explorer.gainLife(choise);
+            	}
+            }
+        } else {
+        	dialog.message("Nie możesz zostać wyleczony, ponieważ jesteś w pełni zdrów bądź masz niewystarczającą ilość złota.");
+        }
+    }
+    
+    private void goodAction(IDialog dialog, Dice dice, Explorer explorer) {
+    	int maxRegainLife = explorer.getMaxRegainLife();
+    	if (maxRegainLife == 0) {
+    		pray(dialog, dice, explorer);
+    	} else {
+        	dialog.message("Możesz zostać uleczony za darmo do ilości punktów Wytrzymałości z początku wędrówki lub możesz się modlić rzucając kością.");
+        	if (dialog.chooseYesNo("Chcesz zostać uleczony?"))
+        		explorer.gainLife(explorer.getMissingLife());
+        	else
+        		pray(dialog, dice, explorer);
+    	}
+    }
+    
+    private void pray(IDialog dialog, Dice dice, Explorer explorer) {
+    	switch(dice.throwDice()) {
+    		case 1:
+    		case 2:
+    		case 3:
+    		case 4:
+    			//1-4 Zostałeś zignorowany
+				dialog.message("Zostałeś zignorowany.");
+				break;
+    		case 5:
+            	//5 Zyskujesz 1 punkt Wytrzymałości
+    			dialog.message("Zyskujesz 1 punkt Wytrzymałości.");
+    			explorer.gainLife();
+    			break;
+    		case 6:
+    			dialog.message("Zyskujesz 1 Czar.");
+    			//explorers.getActualExplorer().gainSpell(board);
+    			break;    		
+    	}
+    }
 }
