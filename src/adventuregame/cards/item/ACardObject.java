23480832a30413a344e1d980279605180bc704c1
Krystian Fejku <przecinek45@o2.pl>
2016-06-10 22:07:59 +0200

    Actualy working on: -Items
    TODO:
    -FieldGetCard
    -Pola
    -Puszcza
    -Rowniny
    -Ruiny
    -Straznik
    -Wzgorza
    
    Done:
    -Gospoda
    -Miasto
    -Las
    -Skały
    -Cmentarz
    -Kapliczk
    -Wioskaa

---
+++ src/adventuregame/cards/item/ACardObject.java
@@ -0,0 +1,17 @@
+package adventuregame.cards.item;
+
+import adventuregame.cards.ACard;
+
+public abstract class ACardObject extends ACard {
+	
+	private final int amountInDeck;
+	
+	public ACardObject(String name, int amountInDeck) {
+		super(name);
+		this.amountInDeck = amountInDeck;
+	}
+	
+	public int getAmountInDeck() {
+		return amountInDeck;
+	}
+}
