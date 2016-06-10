251117fc746941de972b3d5f99fa43b1063dd23f
Krystian Fejku <przecinek45@o2.pl>
2016-06-10 22:13:05 +0200

    Actualy working on: -Items
    TODO:
    -FieldGetCard
    -Pola
    -Puszcza
    -Rowniny
    -Ruiny
    -Straznik
    -Wzgorza
    
    Done:
    -Gospoda
    -Miasto
    -Las
    -Skały
    -Cmentarz
    -Kapliczk
    -Wioskaa

--- src/adventuregame/board/fields/Wioska.java
+++ src/adventuregame/board/fields/Wioska.java
@@ -5,7 +5,15 @@
  */
 package adventuregame.board.fields;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import adventuregame.board.Board;
+import adventuregame.board.fields.miasto.ICitizen;
+import adventuregame.board.fields.miasto.Kowal;
+import adventuregame.board.fields.miasto.Lekarz;
+import adventuregame.board.fields.miasto.Mystic;
+import adventuregame.explorer.Explorer;
 import adventuregame.explorer.Explorers;
 
 /**
@@ -20,8 +28,28 @@ public class Wioska extends Field{
 
     @Override
     public void action(Board board, Explorers explorers) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    	List<ICitizen> citizens = initListWithValidCitizens(explorers.getActualExplorer());
+
+    	String[] citizensNames = new String[citizens.size()];
+    	for (int i = 0; i < citizens.size(); i++) {
+			citizensNames[i] = citizens.get(i).getName();
+		}
+    	int choice = board.getDialog().chooseOption("Możesz odwiedzić:", citizensNames);
+    	citizens.get(choice).Action(board, explorers.getActualExplorer());
     }
     
+    public List<ICitizen> initListWithValidCitizens(Explorer explorer) {
+    	List<ICitizen> citizens = new ArrayList<>();
+    	ICitizen lekarz = new Lekarz();
+    	if (lekarz.isValid(explorer)) 
+    		citizens.add(lekarz);
+    	ICitizen kowal = new Kowal();
+    	if (kowal.isValid(explorer)) 
+    		citizens.add(kowal);
+    	ICitizen mystic = new Mystic();
+    	citizens.add(mystic);
+    	
+    	return citizens;
+    }
     
 }
