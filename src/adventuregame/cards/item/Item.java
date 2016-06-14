3453a57e8e57cf7230c5088bb2924ed6ee2f99ed
Krystian Fejku <przecinek45@o2.pl>
2016-06-14 21:32:37 +0200

    Changed convention for turn action(messy)
    Actualy working on:
    -Items
    
    TODO:
    -FieldGetCard
    -Pola
    -Puszcza
    -Rowniny
    -Ruiny
    -Straznik
    -Wzgorza
    
    Done:
    -Gospoda
    -Miasto
    -Las
    -Skały
    -Cmentarz
    -Kapliczk
    -Wioskaa

---
+++ src/adventuregame/cards/item/Item.java
@@ -0,0 +1,33 @@
+package adventuregame.cards.item;
+
+import adventuregame.cards.ACard;
+
+public abstract class Item extends ACard {
+	
+    private final int amountInDeck;
+	private final int costInTown; //??
+
+    public Item(String name, int amountInDeck) {
+        super(name, 5);
+        this.amountInDeck = amountInDeck;
+        this.costInTown = 0;
+    }
+    
+    public Item(String name, int amountInDeck, int costInTown) {
+        super(name, 5);
+        this.amountInDeck = amountInDeck;
+    	this.costInTown = costInTown;
+    }
+
+    public int getAmountInDeck() {
+        return amountInDeck;
+    }
+    
+    public int getCostInTown() {
+    	return costInTown;
+    }
+    
+    public boolean isWeapon() {
+    	return false;
+    }
+}
