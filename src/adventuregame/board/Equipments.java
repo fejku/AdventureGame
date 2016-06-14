bd57bce2aebb51ec9f104599a3a1004976992892
Krystian Fejku <przecinek45@o2.pl>
2016-06-14 21:34:56 +0200

    Changed convention for turn action(messy)
    Actualy working on:
    -Items
    
    TODO:
    -FieldGetCard
    -Pola
    -Puszcza
    -Rowniny
    -Ruiny
    -Straznik
    -Wzgorza
    
    Done:
    -Gospoda
    -Miasto
    -Las
    -Skały
    -Cmentarz
    -Kapliczk
    -Wioskaa

--- src/adventuregame/board/Equipments.java
+++ src/adventuregame/board/Equipments.java
@@ -3,22 +3,22 @@ package adventuregame.board;
 import java.util.ArrayList;
 import java.util.List;
 
-import adventuregame.cards.item.ACardObject;
+import adventuregame.cards.item.Item;
 import adventuregame.cards.item.Helm;
 import adventuregame.cards.item.Tarcza;
-import adventuregame.cards.item.Topor;
+import adventuregame.cards.item.weapon.Topor;
 import adventuregame.cards.item.Zbroja;
 import adventuregame.cards.item.weapon.Miecz;
 
 public class Equipments {
-    private List<ACardObject> equipments;
+    private List<Item> equipments;
 
     public Equipments() {
         equipments = initEquipments();
     }
 	
-    private List<ACardObject> initEquipments() {
-    	List<ACardObject> equipment = new ArrayList<>();
+    private List<Item> initEquipments() {
+    	List<Item> equipment = new ArrayList<>();
     	
     	equipment.addAll(addEquipment(new Miecz()));
     	equipment.addAll(addEquipment(new Helm()));
@@ -29,13 +29,13 @@ public class Equipments {
     	return equipment;
     }
     
-    public List<ACardObject> getEquipments() {
+    public List<Item> getEquipments() {
     	return equipments;
     }
     
     public int amountOfEquipmentInDeck(Class<?> equipmentType) {
     	int amount = 0;
-    	for (ACardObject equipment: equipments) {
+    	for (Item equipment: equipments) {
             if (equipmentType.isInstance(equipment)) {
                 amount++;
             }
@@ -47,7 +47,7 @@ public class Equipments {
     	return (amountOfEquipmentInDeck(equipmentType) > 0);
     }
     
-    public ACardObject removeEquipmentFromDeck(Class<?> equipmentType) {
+    public Item removeEquipmentFromDeck(Class<?> equipmentType) {
     	int firstObjectNr = -1;
     	
     	for (int i = 0; i < equipments.size(); i++) {
@@ -58,8 +58,8 @@ public class Equipments {
     	return equipments.remove(firstObjectNr);
     }
     
-    private List<ACardObject> addEquipment(ACardObject cardObject) {
-    	List<ACardObject> equipment = new ArrayList<>();
+    private List<Item> addEquipment(Item cardObject) {
+    	List<Item> equipment = new ArrayList<>();
     	
     	for (int i = 0; i < cardObject.getAmountInDeck(); i++)
             equipment.add(cardObject);
