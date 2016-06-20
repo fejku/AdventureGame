8226cb8a07282f3f27a6a139743663ffb477bd49
Krystian Fejku <przecinek45@o2.pl>
2016-06-20 23:22:11 +0200

    Actualy working on: Events cards

---
+++ src/adventuregame/cards/stranger/Magik.java
@@ -0,0 +1,21 @@
+package adventuregame.cards.stranger;
+
+import adventuregame.board.Board;
+import adventuregame.explorer.Explorers;
+
+/**
+ *
+ * @author Fake
+ */
+public class Magik extends Stranger {
+
+    public Magik() {
+        super("Magik", false, false);
+    }
+
+    @Override
+    public void mainAction(Board board, Explorers explorers) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
