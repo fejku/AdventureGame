fd6b3f003e59e89259f72aa2c59bc4065abd4e40
fejku <przecinek45@o2.pl>
2016-06-20 15:06:32 +0200

    Update Zbojcy.java

--- src/adventuregame/cards/events/Zbojcy.java
+++ src/adventuregame/cards/events/Zbojcy.java
@@ -1,9 +1,29 @@
 package adventuregame.cards.events;
 
+import adventuregame.board.Board;
+import adventuregame.board.fields.Field;
+import adventuregame.explorer.Explorers;
+
 /**
  *
  * @author Fake
  */
-public class Zbojcy {
+public class Zbojcy extends Event {
+
+	public Zbojcy() {
+		super("Zbójcy", false);
+	}
+
+	@Override
+	public void mainAction(Board board, Explorers explorers) {
+		board.getDialog().message("Napada cię banda zbójców i rabuje wszystkie twoje Przedmioty i całe złoto. Ukrywają się swój łup w Oazie (połóż tam Karty Przedmiotów i żetony złota), potem wycofują się do swej kryjówki. Odłóż tę Kartę.");
+		
+		int goldAmount = explorers.getActualExplorer().getGold();
+		explorers.getActualExplorer().setGold(0);
+		board.getField(Field.OAZA).setAmountGoldOnField(goldAmount);
+
+		for (int i = 0; i < explorers.getActualExplorer().getItems().size(); i++)
+			board.getField(Field.OAZA).putCardOnField(explorers.getActualExplorer().getItems().remove(i));
+	}
 
 }
