0fc8bef54fa3159af89721622f75ea24e37e95a2
fejku <przecinek45@o2.pl>
2016-06-21 10:36:22 +0200

    Update TrzesienieZiemi.java

--- src/adventuregame/cards/events/TrzesienieZiemi.java
+++ src/adventuregame/cards/events/TrzesienieZiemi.java
@@ -1,6 +1,11 @@
 package adventuregame.cards.events;
 
+import java.util.Iterator;
+
 import adventuregame.board.Board;
+import adventuregame.board.fields.Field;
+import adventuregame.board.fields.Field.Region;
+import adventuregame.cards.ACard;
 import adventuregame.explorer.Explorers;
 
 /**
@@ -15,7 +20,18 @@ public class TrzesienieZiemi extends Event {
 
     @Override
     public void mainAction(Board board, Explorers explorers) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        board.getDialog().message("Krainą, w której się znajdujesz wstrząsa potężne trzęsienie ziemi. Rzuć kostką dla każdej Karty Przygód, która leży w tej Krainie i jest odkryta. Rezultat równy 1 lub 2 oczkom oznacza, że ta Karta wpada w powstałą w ziemi szczelinę należy ją odłożyć.");
+        
+        Region region = board.getField(explorers.getActualExplorer().getActualPosition()).getRegion();
+        
+        for (Field field : board.getFieldsByRegion(region)) {
+        	Iterator<ACard> iteratorCard = field.getCards().iterator();
+        	while (iteratorCard.hasNext()) {
+        		ACard card = iteratorCard.next();
+        		if (board.getDice().throwDice() < 3)
+        			iteratorCard.remove();
+        	}
+        }
     }
 
 }
